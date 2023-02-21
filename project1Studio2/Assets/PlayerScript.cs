8f46a62d79670c011c49c07751905e70b7251562
Aly Sharaf <ali.charaf59@gmail.com>
2023-02-21 15:49:56 +0300

    cloned first version

---
+++ project1Studio2/Assets/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Transform Gun;
+
+    Vector2 direction;
+
+    public GameObject bullet;
+
+    public float bulletSpeed;
+
+    public Transform ShootPoint;
+
+    public Animator gunAnimator;
+
+
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)Gun.position;
+        FaceMouse();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shoot();
+        }
+
+    }
+    void FaceMouse()
+    {
+        Gun.transform.right = direction;
+    }
+
+    void shoot()
+    {
+        GameObject BulletIns = Instantiate(bullet, ShootPoint.position, ShootPoint.rotation);
+        BulletIns.GetComponent<Rigidbody2D>().AddForce(BulletIns.transform.right * bulletSpeed);
+        gunAnimator.SetTrigger("shoot");
+    }
+}

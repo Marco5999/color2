31f6c71f6210fe1aebce416c975ccc27df45fa5e
Aly Sharaf <ali.charaf59@gmail.com>
2023-02-21 17:57:36 +0300

    fixed folder issue

---
+++ project1Studio2/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public GameObject cubeColor;
+    public Color color1, color2, color3;
+
+    public bool isChanged = false;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.tag.Equals("Player")) 
+        {
+            if(isChanged == false)
+            {
+                GetComponent<SpriteRenderer>().color = color1;
+                isChanged = true;
+            }
+            else if(isChanged == true)
+            {
+                GetComponent<SpriteRenderer>().color = color2;
+                isChanged = false;
+            }
+            else if (isChanged == false)
+            {
+                GetComponent<SpriteRenderer>().color = color3;
+                isChanged = true;
+            }
+            //GetComponent<SpriteRenderer>().color = color1; 
+            //GetComponent<SpriteRenderer>().color = Color.red;
+           // Destroy(col.gameObject);
+
+        }
+    }
+    private void Update()
+    {
+        //if(GameObject.FindGameObjectWithTag("Cube").color == Color.red)
+        {
+
+        }
+    }
+    
+
+
+
+}
---
+++ project1Studio2/Assets/Scripts/ScenesLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesLoader : MonoBehaviour
+{
+    public int iLevelToLoad;
+    public string sLevelToLoad;
+
+    public bool useIntegerToLoadLevel = false;
+    AudioSource audioSrc;
+
+    void Start()
+    {
+        //audioSrc = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+
+        if (collisionGameObject.name == "Player")
+        {
+            //audioSrc.Play();
+           // Invoke("LoadScene", 0.5f);
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    void LoadScene()
+    {
+        if (useIntegerToLoadLevel)
+        {
+            SceneManager.LoadScene(iLevelToLoad);
+        }
+
+        else
+        {
+            SceneManager.LoadScene(sLevelToLoad);
+        }
+    }
+}
